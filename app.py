d2164ebf9494a97fbf51a810a7d6c320f8e36586
Felix Fricke <felix@frickefamily.de>
2018-09-11 18:26:05 +0200

    v0.2.2 saving using json instead of pickle

--- app.py
+++ app.py
@@ -422,8 +422,8 @@ def startChrome():
     driver.get("http://127.0.0.1:5000")
 
 if __name__ == "__main__":
-    #t = Thread(target=startChrome)
-    #t.start()
-    #app.run(debug=True)
+    t = Thread(target=startChrome)
+    t.start()
+    app.run(debug=False)
     #app.run(host='192.168.0.77', port=5000, debug=False)
-    app.run(host='10.2.211.39', port=5000, debug=False)
+    #app.run(host='10.2.211.39', port=5000, debug=False)
